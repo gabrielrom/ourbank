6ef127dc1ddd77b78bff72a82c601600f24e9902
gabrielrom <gabrielpessoal626@gmail.com>
2021-06-17 15:33:35 -0300

    :sparkles: feat: add table users on dbcontext

--- src/modules/users/entities/User.cs
+++ src/modules/users/entities/User.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ourbank.entities {
+  [Table("Users")]
   public class User {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
