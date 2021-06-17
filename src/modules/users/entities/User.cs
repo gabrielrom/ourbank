b7fd7663ad99a8de13ed871e0cfe687192ffb589
gabrielrom <gabrielpessoal626@gmail.com>
2021-06-17 15:00:51 -0300

    :art: refactor: add data annotations attr in user

--- src/modules/users/entities/User.cs
+++ src/modules/users/entities/User.cs
@@ -1,10 +1,35 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ourbank.entities {
   public class User {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public string id { get; private set; }
+
+    [Column]
+    [Required]
     public string name { get; set; }
+
+    [Column]
+    [Required]
     public string email { get; set; }
+
+    [Column]
+    [Required]
     public string password { get; set; }
+
+    [Column]
     public string avatar_url { get; set; }
+
+    [Column]
     public string account_id { get; set; }
+
+    public User() {
+      if (String.IsNullOrEmpty(this.id)) {
+        this.id = Convert.ToString(Guid.NewGuid());
+      }
+    }
   }
 }
