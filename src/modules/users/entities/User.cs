0e2e3fb3c1d7b6df82f848703d4e8d28cf8779e0
gabrielrom <gabrielpessoal626@gmail.com>
2021-06-17 00:20:19 -0300

    feat: add user entitie

---
+++ src/modules/users/entities/User.cs
@@ -0,0 +1,8 @@
+namespace ourbank.entities {
+  public class User {
+    public string id { get; private set; }
+    public string name { get; set; }
+    public string email { get; set; }
+    public string password { get; set; }
+  }
+}
