7d5acad15dfa812a6b15875fb9c70461545ceff0
gabrielrom <gabrielpessoal626@gmail.com>
2021-06-18 20:22:56 -0300

    :sparkles: feat: add an existing email validation
    Add an existing email validation and an empty properties validation

--- src/modules/users/useCases/CreateUser/CreateUserService.cs
+++ src/modules/users/useCases/CreateUser/CreateUserService.cs
@@ -1,5 +1,6 @@
 using ourbank.entities;
 using ourbank.Repositories;
+using ourbank.Error;
 
 namespace ourbank.sevices {
   public class CreateUserService {
@@ -11,6 +12,24 @@ namespace ourbank.sevices {
     }
     
     public User execute(ICreateDTO data) {
+      if (
+        string.IsNullOrEmpty(data.name) || 
+        string.IsNullOrEmpty(data.email) || 
+        string.IsNullOrEmpty(data.password)
+      ) {
+        throw new AppError(
+          "The properties name, email and password is required!"
+        );
+      }
+
+      User userAlreadyExists = this._usersRepository.findByEmail(
+        data.email
+      );
+
+      if (userAlreadyExists != null) {
+        throw new AppError("this email is already exits!");
+      }
+
       User user = this._usersRepository.create(new ICreateDTO {
         name = data.name,
         email = data.email,
