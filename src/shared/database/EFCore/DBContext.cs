ae52263865584cbc73c11b17adebaa7d36722267
gabrielrom <gabrielpessoal626@gmail.com>
2021-06-18 17:18:03 -0300

    :bug: fix: fixing a bug of createdAt and updatedAt
    The created_at and updated_at columns were not being generated with the current date and all users always had the same dates

--- src/shared/database/EFCore/DBContext.cs
+++ src/shared/database/EFCore/DBContext.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using ourbank.entities;
 
@@ -6,5 +9,43 @@ namespace ourbank.DBContext {
     public DbSet<User> Users { get; set; }
 
     public DBContext (DbContextOptions<DBContext> options) : base(options) {}
+
+    public override int SaveChanges(bool acceptAllChangesOnSuccess) {
+      OnBeforeSaving();
+      return base.SaveChanges(acceptAllChangesOnSuccess);
+    }
+
+    public override async Task<int> SaveChangesAsync(
+      bool acceptAllChangesOnSuccess, 
+      CancellationToken cancellationToken = default(CancellationToken)
+    ) {
+      OnBeforeSaving();
+      return (await base.SaveChangesAsync(
+        acceptAllChangesOnSuccess, 
+        cancellationToken
+      ));
+    }
+
+    private void OnBeforeSaving() {
+      var entries = ChangeTracker.Entries();
+      var utcNow = DateTime.UtcNow;
+
+      foreach (var entry in entries) {
+        if (entry.Entity is BaseEntity trackable) {
+          switch (entry.State) {
+            case EntityState.Modified:
+              
+              trackable.updated_at = utcNow;
+              entry.Property("created_at").IsModified = false;
+              break;
+
+            case EntityState.Added:
+              trackable.created_at = utcNow;
+              trackable.updated_at = utcNow;
+              break;
+          }
+        }
+      }
+    }
   }
 }
