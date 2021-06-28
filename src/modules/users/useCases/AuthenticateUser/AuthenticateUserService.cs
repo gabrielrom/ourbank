4726a87fa54b33b359a6afa8e1a10d8d15520ca8
gabrielrom <gabrielpessoal626@gmail.com>
2021-06-28 00:07:43 -0300

    :recycle: refact: refactoring the result of avatar field

--- src/modules/users/useCases/AuthenticateUser/AuthenticateUserService.cs
+++ src/modules/users/useCases/AuthenticateUser/AuthenticateUserService.cs
@@ -46,7 +46,7 @@ namespace ourbank.sevices {
         user = new {
           name = user.name,
           email = user.email,
-          avatar = user.avatar_url,
+          avatar = $"https://localhost:3333/avatar/{user.avatar_url}",
           account_id = user.accountId
         },
         token = tokenJWT
